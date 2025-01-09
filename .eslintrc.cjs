817e27b66db6cb2b16ebef9c3779f29d1dd7d760
PhuongHE173077 <phuongddhe173077@fpt.edu.vn>
2025-01-09 08:13:33 +0700

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,4 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- * Sample Eslint config for NodeJS ExpressJS MongoDB project
- */
+
 module.exports = {
   env: { es2020: true, node: true },
   extends: [
@@ -19,7 +14,7 @@ module.exports = {
   plugins: [],
   rules: {
     // Common
-    'no-console': 1,
+    'no-console': 0,
     'no-extra-boolean-cast': 0,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
@@ -28,9 +23,7 @@ module.exports = {
     'no-multiple-empty-lines': 1,
     'space-before-blocks': ['error', 'always'],
     'object-curly-spacing': [1, 'always'],
-    'indent': ['warn', 2],
     'semi': [1, 'never'],
-    'quotes': ['error', 'single'],
     'array-bracket-spacing': 1,
     'linebreak-style': 0,
     'no-unexpected-multiline': 'warn',
