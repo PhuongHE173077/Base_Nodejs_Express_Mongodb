054e057cab03f64e94f18d17698ef529f2967f1c
PhuongHE173077 <phuongddhe173077@fpt.edu.vn>
2025-03-28 20:15:35 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,28 +1,38 @@
-
-
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
-
+import { CONNECT_DB } from './config/mongodb'
+import { env } from './config/environment'
+import cookieParser from 'cookie-parser'
+import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
+import { corsOptions } from './config/cors'
+var cors = require('cors')
 const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-    { id: 'id-2', name: 'Two' },
-    { id: 'id-3', name: 'Three' },
-    { id: 'id-4', name: 'Four' },
-    { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  const hostname = env.HOST
+  const port = env.PORT
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello , I am running at ${hostname}:${port}/`)
-})
+  app.use(cors(corsOptions))
+
+  app.use(express.json())
+
+  app.use(cookieParser())
+
+  app.use(errorHandlingMiddleware)
+
+  app.listen(port, hostname, () => {
+    console.log(`Hello , I am running at ${hostname}:${port}/`)
+  })
+}
+
+
+CONNECT_DB()
+  .then(() =>
+    console.log('Connected database successfully !'))
+  .then(() => START_SERVER())
+  .catch((error) => {
+    console.log(error)
+    process.exit(0)
+  })
+
+
